9e4b5fe2f8a82efc9fbed29e793ac652757ca515
MarcosGad <marcosgad@yahoo.com>
2021-10-25 12:12:07 +0200

    Default Admin User

---
+++ config/admin.php
@@ -0,0 +1,6 @@
+<?php
+return [
+        'admin_name' => env('ADMIN_NAME', ''),
+        'admin_email' => env('ADMIN_EMAIL', ''),
+        'admin_password' =>env('ADMIN_PASSWORD', '') ];
+      
--- database/seeds/AdminTableSeeder.php
+++ database/seeds/AdminTableSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use App\User;
 
 class AdminTableSeeder extends Seeder
 {
@@ -12,10 +13,19 @@ class AdminTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('users')->insert([
-            'name' => 'Admin',
-            'email' => 'admin@admin.com',
-            'password' => bcrypt('123123123'),
-        ]);
+        // DB::table('users')->insert([
+        //     'name' => 'Admin',
+        //     'email' => 'admin@admin.com',
+        //     'password' => bcrypt('123123123'),
+        // ]);
+
+        if(config('admin.admin_name')) {
+            User::firstOrCreate(
+                ['email' => config('admin.admin_email')], [
+                    'name' => config('admin.admin_name'),
+                    'password' => bcrypt(config('admin.admin_password')),
+                ]
+            );
+        }
     }
 }
