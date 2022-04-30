cf22276b082d28c4de8aefe0e82eadd4031172c6
MarcosGad <marcosgad@yahoo.com>
2022-04-30 18:47:56 +0200

    Good End

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,9 +12,17 @@ class DatabaseSeeder extends Seeder
         $this->call(religionTableSeeder::class);
         $this->call(GenderTableSeeder::class);
         $this->call(ParentsTableSeeder::class);
+        $this->call(SpecializationsTableSeeder::class);
+
+        $this->call(ClassroomTableSeeder::class);
+        $this->call(GradeSeeder::class);
+        $this->call(SectionsTableSeeder::class);
+        $this->call(StudentsTableSeeder::class);
+
         //make by Package
         //https://github.com/orangehill/iseed
         //php artisan iseed student_accounts
+
         $this->call(SettingsTableSeeder::class);
     }
 }
---
+++ database/seeds/GradeSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\Models\Grade;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class GradeSeeder extends Seeder
+{
+    public function run()
+    {
+        DB::table('grades')->delete();
+        $grades = [
+            ['en'=> 'Primary stage', 'ar'=> 'المرحلة الابتدائية'],
+            ['en'=> 'middle School', 'ar'=> 'المرحلة الاعدادية'],
+            ['en'=> 'High school', 'ar'=> 'المرحلة الثانوية'],
+        ];
+
+        foreach ($grades as $grade) {
+            Grade::create(['Name' => $grade]);
+        }
+
+    }
+}
